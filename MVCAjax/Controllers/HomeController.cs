132d3a35f509ddeeefa3b108d5d53e7674cb63dc
Wissen <Wissen@WISSEN-PC>
2017-10-02 10:55:07 +0300

    Kurulum

---
+++ MVCAjax/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Ornek()
+        {
+            return "Merhaba";
+        }
+        public ActionResult OrnekHtml()
+        {
+            return View();
+        }
+        public JsonResult OrnekJson()
+        {
+            return Json(new { status = true, data = "Sayfa basariyla cagrildi" },JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
